bfcdda4fc2a7ddc494284d827311bbc004167e19
Elio Cortes <elio.cortes3000@gmail.com>
2022-03-08 17:22:23 -0500

    hidden brand

--- js/scriptCode.js
+++ js/scriptCode.js
@@ -4,6 +4,9 @@ function togleMobile() {
   link.classList.toggle('open');
   const menuImg = document.querySelector('.hamburguer');
   menuImg.classList.toggle('hamburg');
+  const brand = document.querySelector('.nick');
+  brand.classList.toggle('hideBrand');
+ 
 }
 
 link.addEventListener('click', togleMobile);
