dd344b1ea960f45917b234cf56425176bd7f8da3
Elio Cortes <elio.cortes3000@gmail.com>
2022-03-08 22:57:39 -0500

    solved PR issues

--- js/scriptCode.js
+++ js/scriptCode.js
@@ -6,7 +6,6 @@ function togleMobile() {
   menuImg.classList.toggle('hamburg');
   const brand = document.querySelector('.nick');
   brand.classList.toggle('hideBrand');
- 
 }
 
 link.addEventListener('click', togleMobile);
