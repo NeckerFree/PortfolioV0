6d7d99402005e443f0201a9fbd90ba9241c05f8f
Elio Cortes <elio.cortes3000@gmail.com>
2022-03-11 17:12:07 -0500

    Solved linters error

--- js/scriptCode.js
+++ js/scriptCode.js
@@ -13,63 +13,59 @@ function togleMobile() {
 }
 
 function storageAvailable(type) {
-  var storage;
+  let storage;
   try {
-      storage = window[type];
-      var x = '__storage_test__';
-      storage.setItem(x, x);
-      storage.removeItem(x);
-      return true;
-  }
-  catch(e) {
-      return e instanceof DOMException && (
-          // everything except Firefox
-          e.code === 22 ||
+    storage = window[type];
+    const x = '__storage_test__';
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (e) {
+    return e instanceof DOMException && (
+    // everything except Firefox
+      e.code === 22
           // Firefox
-          e.code === 1014 ||
+          || e.code === 1014
           // test name field too, because code might not be present
           // everything except Firefox
-          e.name === 'QuotaExceededError' ||
+          || e.name === 'QuotaExceededError'
           // Firefox
-          e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+          || e.name === 'NS_ERROR_DOM_QUOTA_REACHED')
           // acknowledge QuotaExceededError only if there's something already stored
-          (storage && storage.length !== 0);
+          && (storage && storage.length !== 0);
   }
 }
 
 function populateContactForm() {
- 
-  var data = { fullname: fullName.value, email: email.value, comments: comments.value }
-  var contactData = JSON.stringify(data);
+  const data = { fullname: fullName.value, email: email.value, comments: comments.value };
+  const contactData = JSON.stringify(data);
   localStorage.setItem('contactData', contactData);
 }
 
-function setContactForm(){
-   const contactData=localStorage.getItem('contactData');
-   var data=JSON.parse(contactData);
-   fullName.value = data.fullname;
-   email.value = data.email;
+function setContactForm() {
+  const contactData = localStorage.getItem('contactData');
+  const data = JSON.parse(contactData);
+  fullName.value = data.fullname;
+  email.value = data.email;
   comments.value = data.comments;
 }
 
 link.addEventListener('click', togleMobile);
 
-form.addEventListener('submit', (event) => {
+form.addEventListener('submit', () => {
   if (storageAvailable('localStorage')) {
-      populateContactForm();
-  }
-  else {
-   alert ('Local Storage not avalailable');
+    populateContactForm();
+  } else {
+    alert('Local Storage not avalailable');
   }
 });
 
-window.addEventListener('load', (event) => {
+window.addEventListener('load', () => {
   if (storageAvailable('localStorage')) {
     if (localStorage.getItem('contactData')) {
       setContactForm();
     }
+  } else {
+    alert('Local Storage not avalailable');
   }
-  else {
-   alert ('Local Storage not avalailable');
-  }
-} )
+});
